ce1dfa761a27b9278b684151d1b602094ddbe387
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2025-02-22 16:52:05 -0300

    construção da classe ClienteBuilder

---
+++ SistemaAdaCommerce/src/ClienteBuilder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public abstract class ClienteBuilder {
+
+    public static Cliente criarCliente() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o nome do cliente: ");
+        String nome = scanner.nextLine();
+        System.out.print("Digite o documento do cliente: ");
+        String documento = scanner.nextLine();
+        return new Cliente(nome, documento);
+    }
+
+}
