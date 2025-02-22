da02fee476fc920b45cde10702290fa73a9939a1
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2025-02-22 16:51:31 -0300

    construção da classe Cliente

---
+++ SistemaAdaCommerce/src/Cliente.java
@@ -0,0 +1,44 @@
+import java.util.UUID;
+
+public class Cliente {
+
+    private UUID id;
+    private String nome;
+    private String documento;
+
+    public Cliente(String nome, String documento) {
+        this.id = UUID.randomUUID();
+        this.nome = nome;
+        this.documento = documento;
+    }
+
+    public Cliente(UUID idCliente, String nome, String documento) {
+        this.id = idCliente;
+        this.nome = nome;
+        this.documento = documento;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+}
