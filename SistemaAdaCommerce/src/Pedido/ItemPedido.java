3b833810d541bf7aa9df2b25c3cf39f772d1ad28
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2025-03-01 11:46:11 -0300

    Criação da classe ItemPedido

---
+++ SistemaAdaCommerce/src/Pedido/ItemPedido.java
@@ -0,0 +1,32 @@
+package Pedido;
+
+import Produto.Produto;
+
+public class ItemPedido {
+
+    private Produto produto;
+    private int quantidade;
+    private double precoVenda;
+
+    public ItemPedido(Produto produto, int quantidade, double precoVenda) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoVenda = precoVenda;
+    }
+
+    public Produto getProduto() {
+        return this.produto;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public double getPrecoVenda() {
+        return this.precoVenda;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade; }
+}
+
